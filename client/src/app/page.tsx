ea3746770aaf3403422675dbd50659f6780fbdda
Alessandro Cruz <alessandrorafaelcruz@gmail.com>
2025-03-19 13:52:43 +0800

    Project structure and Styling

---
+++ client/src/app/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+async function loader() {
+  const path = "/api/home-page";
+  const BASE_URL = "http://localhost:1337";
+  const url = new URL(path, BASE_URL)
+
+  const response = await fetch(url.href);
+  const data = await response.json();
+
+  return { ...data.data };
+}
+
+async function Home() {
+  const data = await loader();
+  console.log(data);
+
+  return (
+    <div>
+      <h1>{data.title}</h1>
+      <p>{data.description}</p>
+    </div>
+  )
+}
+
+export default Home
